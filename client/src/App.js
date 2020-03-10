20d91562f5e2f9d145d65b5e1176b891d14b6c32
Bobby Liu <bobbyliu@Bobbys-MacBook.local>
2020-03-10 10:06:35 -0400

    add

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>butthole</h1>
+        <h1>You son of a bithc, you better accept my change bitch</h1>
+        <p>hello Im a me!! woohoo</p>
       </div>
     )
   }
